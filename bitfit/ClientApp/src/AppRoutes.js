a1b95214791684bb41aac07a49239d0017b87b17
pntrgergo <pntrgergo@gmail.com>
2023-01-12 15:01:24 +0100

    Feat: Recipe route update to React

--- bitfit/ClientApp/src/AppRoutes.js
+++ bitfit/ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
 import { NavMenu } from "./components/NavMenu";
+import { Recipes } from "./components/Recipes"
 
 const AppRoutes = [
     {
@@ -8,12 +9,16 @@ const AppRoutes = [
     element: <Home />
     },
     {
-        path: '/navmenu',
-        element: <NavMenu />
+    path: '/navmenu',
+    element: <NavMenu />
     },
     {
     path: '/foods',
     element: <FetchData />
+    },
+    {
+    path: '/recipes',
+    element: <Recipes />
     }
 ];
 
---
+++ bitfit/ClientApp/src/components/Recipes.js
@@ -0,0 +1,60 @@
+﻿import React, { Component } from 'react';
+
+export class Recipes extends Component {
+    static displayName = Recipes.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { recipes: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.getSampleRecipes();
+    }
+
+    static renderRecipes(recipes) {
+        return (
+            <div className="container-fluid" >
+                {recipes.map((recipe) => (
+                <div className='card'>
+                    <div className="card-header">{recipe.title}</div>
+                        <div className="card-body">
+                            <ul>
+                            <li>{recipe.ingredients}</li>
+                            <li>{recipe.servings}</li>
+                            <li>{recipe.instructions}</li>
+                            </ul>
+                    </div>
+                </div>
+                ))}
+                </div>
+        );
+            
+    }
+
+    async getSampleRecipes() {
+        const url = "https://api.api-ninjas.com/v1/recipe?query=chicken";
+        fetch(url, {
+            method: "GET",
+            withCredentials: true,
+            headers: {
+                "X-Api-Key": "rjMn+tsZm073LSFbqMeumg==tUY5fuxhyNZqNik8",
+            }
+        })
+            .then(resp => resp.json())
+            .then(data => this.setState({ recipes: data, loading: false }))
+    }
+
+
+    render() {
+        let contents = Recipes.renderRecipes(this.state.recipes);
+
+        return (
+            <div>
+                <h1 id="tabelLabel" >This be da recipes m8</h1>
+                <p>This component demonstrates fetching recipes from the server.</p>
+                {contents}
+            </div>
+        );
+    }
+}
