a63847104113aa875ec011505dab1f960d15c17f
hajas97 <hajastomi97@gmail.com>
2023-01-12 17:01:18 +0100

    feat: add button.js

--- bitfit/ClientApp/src/components/Dashboard.js
+++ bitfit/ClientApp/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 ﻿import React, { Component } from 'react';
 import profilePicture from '../images/ProfilePicture.jpeg';
+import Button from './Button'
 
 export class Dashboard extends Component {
     static displayName = Dashboard.name;
@@ -14,9 +15,14 @@ export class Dashboard extends Component {
                     <h1>Welcome user!</h1>
                 </div>
                 <div>
-                    <img style={{width:"160px", height:"160px", borderRadius:"80px"} } src={profilePicture} alt="profilePicture"/>
+                    <img style={{ width: "160px", height: "160px", borderRadius: "80px" }} src={profilePicture} alt="profilePicture" />
+                </div>
+                <div>
+                    <br></br>
+                    <Button color="#1b6ec2" className="nav-link text-dark" text="Click here to add profile picture" />
                 </div>
             </div>
         )
     }
 }
+export default Dashboard;
