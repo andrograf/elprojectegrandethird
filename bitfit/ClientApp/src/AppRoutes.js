5f3616271b5c8617313ddb8674351db5c559ee52
robert <rnagy89@gmail.com>
2023-01-12 16:07:55 +0100

    Feat: Add Article component

--- bitfit/ClientApp/src/AppRoutes.js
+++ bitfit/ClientApp/src/AppRoutes.js
@@ -1,6 +1,7 @@
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
 import { NavMenu } from "./components/NavMenu";
+import { Articles } from './components/Articles';
 
 const AppRoutes = [
     {
@@ -14,6 +15,10 @@ const AppRoutes = [
     {
     path: '/foods',
     element: <FetchData />
+    },
+    {
+    path: '/articles',
+    element: <Articles />
     }
 ];
 
