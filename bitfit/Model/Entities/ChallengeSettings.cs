19bc41f9a2b56e5ffa094779dfba3049479af71c
Róbert Nagy <roister@windowslive.com>
2023-02-08 14:25:02 +0100

    Feat: Add ChallengeSettings class //now chart generation is handled on backend, via setting provided from front-end react form

---
+++ bitfit/Model/Entities/ChallengeSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace bitfit.Model.Entities
+{
+    public class ChallengeSettings
+    {
+        public string ChallengeType { get; set; }
+        public string DietPlan { get; set; }
+    }
+}
---
+++ bitfit/Utilities/Util.cs
@@ -0,0 +1,12 @@
+﻿using bitfit.Model.Entities;
+
+namespace bitfit.Utilities
+{
+    public class Util
+    {
+        internal void CalculateDailyCalories(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
